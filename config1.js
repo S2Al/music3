d6f8231b163d13c72a9734573d58788181a0a692
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-13 13:52:09 +0200

    Add files via upload

---
+++ config1.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'التوكين'; // حط توكن البوت هنا
+
+exports.PREFIX = 'البريفكس';//برفكس البوت هو يلي بيشغل لاغاني
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لا تغيره
