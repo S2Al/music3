0502735e4e551ad01505b8d21d5a3088d84e0cc0
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-13 14:01:49 +0200

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,4 +1,4 @@
-exports.TOKEN = process.env.BOT_TOKEN; // حط توكن البوت هنا
+exports.TOKEN = 'process.env.BOT_TOKEN'; // حط توكن البوت هنا
 
 exports.PREFIX = '+';//برفكس البوت هو يلي بيشغل لاغاني
 
